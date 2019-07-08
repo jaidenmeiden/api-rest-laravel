af0438d76b73eb0669790b51b971d341f280b973
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-07-08 10:10:37 -0500

    Recoger los datos del usario por post

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,15 +12,25 @@ class UserController extends Controller
 
     public function register(Request $request) {
         /*
-         * Pasos:
+         *
          * Un API RESTful debe devolver un JSON, si no es así, es un servicio web normal
          *
-         * 1. Recoger los datos del uusario por post
-         * 2. Validar datos
-         * 3. Cifrar la contraseña
-         * 4. Comprobar si el suario existe ya (Duplicado)
-         * 5. Crear el usuario
+         * Pasos:
          */
+        //1. Recoger los datos del usario por post
+        $json = $request->input('json', null);
+        //var_dump($json);
+        $params = json_decode($json);//Objeto
+        //var_dump($params);
+        //var_dump($params->name);
+        $params_array = json_decode($json, true);//Array
+        //var_dump($params_array);
+        //var_dump($params_array['name']);
+
+        //2. Validar datos
+        //3. Cifrar la contraseña
+        //4. Comprobar si el suario existe ya (Duplicado)
+        //5. Crear el usuario
         $data = array(
             'status' => 'error',
             'code'   => 404,
