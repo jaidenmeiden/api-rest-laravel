ee647cad3f07b53427958d731cfa47efb3d2613c
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-07-08 17:17:52 -0500

    Obtener categorías

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,10 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Category;
 
 class CategoryController extends Controller
 {
-    public function pruebas() {
-        return "Acción de pruebas de CATEGORY-CONTROLLER";
+    public function index() {
+        $categories = Category::all();
+
+        return \response()->json([
+            'code' => 200,
+            'status' => 'success',
+            'categories' => $categories
+        ]);
+    }
+
+    public function show($id) {
+        $category = Category::find($id);
+
+        if(is_object($category)) {
+            $data = array(
+                'code' => 200,
+                'status' => 'success',
+                'category' => $category
+            );
+        } else {
+            $data = array(
+                'status' => 'error',
+                'code' => 404,
+                'message' => 'La categoría no existe'
+            );
+        }
+
+        return \response()->json($data, $data['code']);
     }
 }
