940ad4fd2059d2cd180e8fd0ae70c75c9f967845
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-07-11 12:24:06 -0500

    Guardar un post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Post;
+use App\Helpers\JwtAuth;
 
 class PostController extends Controller
 {
@@ -44,5 +45,59 @@ class PostController extends Controller
         return response()->json($data, $data['code']);
     }
 
+    public function store(Request $request) {
+        //1. Recoger los datos
+        $json = $request->input('json', null);
+        $params = json_decode($json);//Object
+        $params_array = json_decode($json, true);//Array
+
+        if(!empty($params_array)) {
+            //2. Conseguir usuario identificado
+            $jwtAuth = new JwtAuth();
+            $token = $request->header('Authorization', null);
+            $user = $jwtAuth->checkToken($token, true);
+
+            //3. Validar los datos
+            $validate = \Validator::make($params_array, [
+                'title' => 'required',
+                'content' => 'required',
+                'category_id' => 'required',
+                'image' => 'required',
+            ]);
+
+            //4. Validar la contraseña
+            if($validate->fails()) {
+                $data = array(
+                    'status' => 'error',
+                    'code' => 400,
+                    'message' => 'No se ha guardado el post'
+                );
+            } else {
+                //3. Guardar el post
+                $post = new Post();
+                $post->title = $params_array['title'];
+                $post->content = $params_array['content'];
+                $post->category_id = $params_array['category_id'];
+                $post->user_id = $user->sub;
+                $post->image = $params->image;
+                $post->save();
+
+                //4. Devolver resultado
+                $data = array(
+                    'code' => 200,
+                    'status' => 'success',
+                    'category' => $post
+                );
+            }
+        } else {
+            $data = array(
+                'status' => 'error',
+                'code' => 404,
+                'message' => 'No has enviado ningún post'
+            );
+        }
+
+        return \response()->json($data, $data['code']);
+    }
 
 }
