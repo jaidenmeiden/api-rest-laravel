7317464ab359a977837c02920aaf9202795eecdf
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-07-11 17:07:46 -0500

    Listar artículos por usuario y categoría

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,13 @@ class PostController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('api.auth', ['except' => ['index', 'show', 'getImage']]);
+        $this->middleware('api.auth', ['except' => [
+            'index',
+            'show',
+            'getImage',
+            'getPostsByCategory',
+            'getPostsByUser'
+        ]]);
     }
 
 
@@ -240,5 +246,23 @@ class PostController extends Controller
             return response()->json($data, $data['code']);
         }
 
+    }
+
+    public function getPostsByCategory($id) {
+        $posts = Post::where('category_id', $id)->get();
+
+        return response()->json([
+            'status' => 'success',
+            'posts' => $posts
+        ], 200);
+    }
+
+    public function getPostsByUser($id) {
+        $posts = Post::where('user_id', $id)->get();
+
+        return response()->json([
+            'status' => 'success',
+            'posts' => $posts
+        ], 200);
     }
 }
